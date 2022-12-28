fd04d4e6330d3da7e686bea88bb7ab4c715a1f43
Alex Torres <alextorressk@gmail.com>
2022-12-28 06:29:57 -0500

    Change dist directory

--- pages/api/posts.js
+++
@@ -1,6 +0,0 @@
-import { getAllFilesFrontMatter } from '../../lib/mdx'
-
-export async function getAllPostsAPI(req, res) {
-    const allPosts = await getAllFilesFrontMatter()
-    res.status(200).json(allPosts)
-}
