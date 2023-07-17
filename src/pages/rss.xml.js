15c6bce96e3ed810ef6918ba0aa6876dcb6bfc87
Alex Torres <alextorressk@gmail.com>
2023-07-16 23:27:08 -0500

    Fix entrypoints

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 
 export async function get(context) {
   const blog = await getCollection("blog");
