70070fc758015067905115cb8a226c576f7a1bac
Alex Torres <alextorressk@gmail.com>
2023-09-01 12:21:57 -0500

    feat(tx): :lipstick: Added transitions to blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -19,9 +18,8 @@ const markdownConfig = {
 export default defineConfig({
   site: 'https://alextrs.dev',
   markdown: markdownConfig,
-  integrations: [sitemap(), tailwind(), preact(), mdx(markdownConfig), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-    cacheDir: './.cache/image',
-    logLevel: 'debug'
-  })]
+  image: {
+    domains: ['https://amazonaws.com']
+  },
+  integrations: [sitemap(), tailwind(), preact(), mdx(markdownConfig)]
 })
